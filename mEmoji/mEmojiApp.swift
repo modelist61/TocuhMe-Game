01c5ec03cccaeb90acf21f5c25b2dac2d9c5d926
Tokarev Dmitry <modelist61@bk.ru>
2021-01-08 11:00:01 +0300

    Initial Commit

---
+++ mEmoji/mEmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  mEmojiApp.swift
+//  mEmoji
+//
+//  Created by Dmitry Tokarev on 08.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct mEmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
